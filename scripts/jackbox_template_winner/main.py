8661f35abe919edaf60b3da2d93689b7f8a0326b
DragonWF <plarisanmarc@gmail.com>
2022-01-23 02:55:51 +0800

    Made the main structure for execution

--- scripts/jackbox_template_winner/main.py
+++ scripts/jackbox_template_winner/main.py
@@ -1,17 +1,54 @@
 from rich.console import Console
 
 console = Console()
+current_game = ""
 
 
-def input_line(current_game):
-    pass
+def write_new_title():
+    game_title = input("Game Title: ").lower()
+
+
+def input_line():
+    global current_game
+    output = ""
+    if not current_game:
+        write_new_title()
+    else:
+        while True:
+            same_title = input("Same Game? (y/n) ").lower().strip()
+            if same_title == "y" or same_title == "yes":
+                pass
+            elif same_title == "n" or same_title == "no":
+                write_new_title()
+            else:
+                console.print("Invalid Option")
 
 
 def main():
     output = ""
-    current_game = ""
+    lines = []
+
     while True:
-        pass
+        try:
+            session_number = int(input("Enter session number: "))
+            lines.append(f"Jackbox Play Session #{session_number}\n")
+            lines.append("**Winners from this session:**\n")
+            break
+        except ValueError:
+            console.print("Invalid session number!")
+
+    while True:
+        line_input = input_line()
+        lines.append(line_input)
+        break
+
+    for line in lines:
+        output += f"{line}\n"
+
+    with open("output.txt", "w") as file:
+        file.write(output)
+
+    return output
 
 
 console.print(main())
