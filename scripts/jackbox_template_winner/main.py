a96300f16214af98161098b6a20c951a9bfed642
DragonWF <plarisanmarc@gmail.com>
2022-01-23 02:29:18 +0800

    Added file

---
+++ scripts/jackbox_template_winner/main.py
@@ -0,0 +1,17 @@
+from rich.console import Console
+
+console = Console()
+
+
+def input_line(current_game):
+    pass
+
+
+def main():
+    output = ""
+    current_game = ""
+    while True:
+        pass
+
+
+console.print(main())
