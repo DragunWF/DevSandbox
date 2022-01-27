7b9e61db6d9ec09df9abaa4fcc120eb661ce5080
DragonWF <plarisanmarc@gmail.com>
2022-01-28 02:34:10 +0800

    Added styling and the ability to type out an extra command

--- scripts/discord/auto_cookie_work.py
+++ scripts/discord/auto_cookie_work.py
@@ -1,10 +1,14 @@
-from pynput.keyboard import Key, Controller
+import random
+import math
 from time import sleep
+from pynput.keyboard import Key, Controller
+from rich.console import Console
 
 keyboard = Controller()
+console = Console()
 
 # Settings
-iterations = 100
+iterations = 250
 command = "!work"
 
 
@@ -14,10 +18,17 @@ def send_message():
 
 
 def main():
+    colors = ("red", "green", "yellow", "blue",
+              "magenta", "red", "cyan", "orange3")
     for x in range(iterations):
         keyboard.type(command)
         send_message()
+        console.print(f"Iteration: {x + 1}",
+                      style=f"bold {random.choice(colors)}")
         sleep(60)
+        if math.floor((x + 1) % 10) == 0:
+            keyboard.type("!dep all")
+            send_message()
 
 
 sleep(3)
