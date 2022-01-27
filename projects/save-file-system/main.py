8c1bec56666f2ec751cf2eab237294106f16d7e5
DragonWF <plarisanmarc@gmail.com>
2022-01-28 01:32:48 +0800

    Added the base logic for checking mouse hover

--- projects/save-file-system/main.py
+++ projects/save-file-system/main.py
@@ -1,4 +1,6 @@
 import pygame
+import json
+import random
 from sys import exit
 
 pygame.init()
@@ -12,7 +14,7 @@ button_delay = pygame.USEREVENT + 1
 
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self, event_type):
+    def __init__(self, event_type, x_pos, y_pos):
         self.event = event_type
         frame_1 = pygame.image.load(
             "projects/save-file-system/assets/blue_button.png")
@@ -21,21 +23,49 @@ class Button(pygame.sprite.Sprite):
         frame_3 = pygame.image.load(
             "projects/save-file-system/assets/red_button_pushed.png")
         self.frames = [frame_1, frame_2, frame_3]
+        self.frames = list(
+            map(lambda frame: pygame.transform.scale2x(frame), self.frames))
         self.image = self.frames[0]
-        self.rect = self.image.get_rect(center=(400, 200))
+        self.rect = self.image.get_rect(center=(x_pos, y_pos))
+
+    def increase_gold():
+        return random.randint(25, 100)
+
+    def decrease_gold():
+        return -random.randint(25, 100)
+
+    def save_file():
+        pass
+
+    def load_file():
+        pass
 
     def on_pressed(self):
-        events = ("increase", "decrease", "save", "load")
+        events = {"increase": self.increase_gold, "decrease": self.decrease_gold,
+                  "save": self.save_file, "load": self.load_file}
+        return events[self.event]()
 
     def check_hover(self, pos, pressed):
         if self.rect.collidepoint(pos):
             self.image = self.frames[1]
+            if pressed:
+                self.on_pressed()
+        else:
+            self.image = self.frames[0]
 
     def update(self, mouse_pos, mouse_pressed):
         self.check_hover(mouse_pos, mouse_pressed)
 
 
-def check_collisions():
+class PlayerCurrency:
+    def __init__(self):
+        self.gold = 0
+        self.increase_pressed_amount = 0
+        self.decrease_pressed_amount = 0
+        self.saved_amount = 0
+
+
+def check_collisions(mouse_pos, mouse_pressed):
     pass
 
 
