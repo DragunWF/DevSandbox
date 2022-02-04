b316d26fdf304d1b95da27bfd6b262795432d905
DragonWF <plarisanmarc@gmail.com>
2022-02-04 13:46:18 +0800

    Added function that checks time

--- scripts/school/auto_join_zoom.py
+++ scripts/school/auto_join_zoom.py
@@ -4,12 +4,8 @@ from datetime import datetime
 from rich.console import Console
 
 console = Console()
-
-meetings = {"subject": ("id", "password")}
-positions = {"desktop": (1359, 746), "zoom_icon": (112, 289),
-             "join_meeting": (537, 313), "meeting_id": (700, 495),
-             "enter_meeting": (686, 496), "close_zoom": (1109, 41)}
-actions = tuple([x for x in positions])
+meetings = {"subject1": ("id", "password"), "subject2": (
+    "id", "password"), "subject3": ("id", "password")}
 
 
 def input_text_field(content, input_type):
@@ -19,7 +15,11 @@ def input_text_field(content, input_type):
 
 
 def enter_meeting():
-    for action in actions:
+    positions = {"desktop": (1359, 746), "zoom_icon": (112, 289),
+                 "join_meeting": (537, 313), "meeting_id": (700, 495),
+                 "enter_meeting": (686, 496), "close_zoom": (1109, 41)}
+
+    for action in tuple([x for x in positions]):
         delay = 0.8
         if action != "zoom_icon":
             if action in ("meeting_id", "enter_meeting"):
@@ -33,6 +33,24 @@ def enter_meeting():
         sleep(delay)
 
 
+def check_time():
+    day_of_week = ("sunday", "monday", "tuesday", "wednesday",
+                   "thursday", "friday", "saturday")
+    start = datetime(2022, 1, 1)
+    now = datetime.now()
+    duration = now - start
+
+    day = day_of_week[(duration.days - 1) % 7]
+    hour = str(now).split()[1].split(".")[0]
+
+    return day, hour
+
+
+def main():
+    while True:
+        pass
+
+
 if __name__ == "__main__":
     try:
         sleep(1)
@@ -41,3 +59,7 @@ if __name__ == "__main__":
                       style="bold green")
     except Exception:
         console.print(str(Exception), style="bold red")
+
+# 12:00:00
+# 12:55:00
+# 13:55:00
