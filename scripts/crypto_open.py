475661d4e56e5125cf37106cf659e9552c9ab4e4
DragonWF <plarisanmarc@gmail.com>
2022-03-01 16:53:44 +0800

    Optimizations and fixes

--- scripts/crypto_open.py
+++ scripts/crypto_open.py
@@ -1,6 +1,6 @@
 import pyautogui
-from time import sleep
 from colored import fg
+from time import sleep
 
 green = fg("light_green")
 red = fg("light_red")
@@ -11,12 +11,15 @@ def execute_clicks():
     positions = {"FirstCrypto": (1310, 20), "SecondCrypto": (1311, 114),
                  "ThirdCrypto": (1312, 203), "FirstIcon": (569, 500),
                  "OtherIcons": (620, 469), "Desktop": (1359, 746)}
-    actions = ("FirstCrypto", "FirstIcon", "Desktop",
+    actions = ("Desktop",
+               "FirstCrypto", "FirstIcon", "Desktop",
                "SecondCrypto", "OtherIcons", "Desktop",
                "ThirdCrypto", "OtherIcons", "Desktop")
+
     for position in actions:
         pyautogui.doubleClick(positions[position])
-        sleep(0.1)
+        sleep(1.5 if position in ("FirstCrypto",
+              "SecondCrypto", "ThirdCrypto") else 0.1)
 
 
 def main():
