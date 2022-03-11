a56bf29e2320abc0778fb71a338e9218f5b7347f
DragonWF <plarisanmarc@gmail.com>
2022-03-11 15:35:23 +0800

    Added main logic

--- scripts/skyrim/auto_sleep.py
+++ scripts/skyrim/auto_sleep.py
@@ -1,10 +1,41 @@
 import pyautogui
+import keyboard
+import asyncio
 from time import sleep
+from sys import exit
+
+sleep_switch = False
+sleeping = False
+
+
+def sleep_in_bed():
+    global sleeping
+    positions = {"sleep": (), "enter": ()}
+    sleeping = True
+
+    pyautogui.press("e")
+    for position in tuple([x for x in positions]):
+        pyautogui.click(positions[position])
+        sleep(0.25)
+        
+    sleep(25)
+    sleeping = False
 
 
 def main():
-    pass
+    global sleep_switch
+    while True:
+        if keyboard.is_pressed("o"):
+            sleep_switch = True
+        elif keyboard.is_pressed("p"):
+            sleep_switch = False
+        elif keyboard.is_pressed("l"):
+            exit()
+
+        if sleep_switch:
+            sleep_in_bed()
+        sleep(0.025)
 
 
 if __name__ == "__main__":
-    pass
+    main()
