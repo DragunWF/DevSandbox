579bf8f9c00de7accee4197f1df652feef61d0c8
dragunwf <plarisanmarc@gmail.com>
2022-10-29 14:20:27 +0800

    Updated selection_sort.py

--- scripts/challenges/selection_sort.py
+++ scripts/challenges/selection_sort.py
@@ -6,9 +6,13 @@ def selection_sort(unsorted: list):
     min_num_index = None
     for i in range(list_count):
         min_num_index = i
+        no_swaps = True
         for j in range(i, list_count):
             if unsorted[j] < unsorted[min_num_index]:
                 min_num_index = j
+                no_swaps = False
+        if no_swaps:
+            break
         temp = unsorted[i]
         unsorted[i] = unsorted[min_num_index]
         unsorted[min_num_index] = temp
