870eaf4c135655ed87d1502cb155f46dfd8a2003
Marc Plarisan <plarisanmarc@gmail.com>
2022-11-23 21:25:40 +0800

    Updated change_image_location.py

--- scripts/uncategorized/change_image_location.py
+++ scripts/uncategorized/change_image_location.py
@@ -26,8 +26,8 @@ def get_images(directory: str) -> list:
     files = os.listdir(directory)
     image_extensions = ("png", "jpg", "webp")
     for file in files:
-        cog = file.split(".")
-        if cog[-1] in image_extensions:
+        file_extension = file.split(".")[-1].lower()
+        if file_extension in image_extensions:
             output.append(file)
     return output
 
