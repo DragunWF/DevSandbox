7d2253263649bafbab378d7aae65de8d211d517c
dragunwf <plarisanmarc@gmail.com>
2023-01-14 23:05:11 +0800

    Updated kill_distractions.py

--- scripts/school/kill_distractions.py
+++ scripts/school/kill_distractions.py
@@ -3,12 +3,14 @@ from time import sleep
 
 
 def main():
+    distraction_processes = ("spotify", "discord")
     iterations = 0
-    command = "powershell.exe ps spotify | kill"
     while True:
         iterations += 1
-        subprocess.run(command)
-        print(f"[Command Successfully Ran] ({iterations}): ps spotify | kill")
+        for process in distraction_processes:
+            command = f"powershell.exe ps {process} | kill"
+            subprocess.run(command)
+        print(f"[Command Successfully Ran]: Iteration: {iterations}")
         sleep(15)
 
 
