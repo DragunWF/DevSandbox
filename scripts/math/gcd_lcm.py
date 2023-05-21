6b7db91fb926125a27c7515842073a2ec0f45dfa
Marc Plarisan <plarisanmarc@gmail.com>
2023-05-21 22:34:48 +0800

    Create gcd_lcm.py

---
+++ scripts/math/gcd_lcm.py
@@ -0,0 +1,19 @@
+import random
+from rich import print
+
+
+class MathGame:
+    def __init__(self, problems: int):
+        self.__chosen_numbers = []
+        self.__RANGE = (15, 1000)
+        for i in range(problems):
+            random_num = random.randint(self.__RANGE[0],self.__RANGE[1])
+            if not random_num in self.__chosen_numbers:
+                self.__chosen_numbers.append(random_num)
+
+    def play() -> None:
+        pass
+
+
+if __name__ == "__main__":
+    MathGame().play(0)
