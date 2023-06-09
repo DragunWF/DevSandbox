fcf5b0a23c04cd5cd0fca8a71ebd89fe0a5f5a8e
dragunwf <plarisanmarc@gmail.com>
2023-06-09 16:45:18 +0800

    Update mock_interview.py

--- scripts/school/mock_interview.py
+++ scripts/school/mock_interview.py
@@ -5,7 +5,27 @@ voice_engine = pyttsx3.init("sapi5")
 voices = voice_engine.getProperty("voices")
 voice_engine.setProperty("voice", voices[0].id)
 
-questions = ["Are you gae", "wanna have sex?"]
+questions = ["Tell me something about yourself",
+             "How did you hear about this position?",
+             "Why do you want to work here?",
+             "Why did you decide to apply for this position?",
+             "What is your greatest strength?",
+             "What are your strengths and weaknesses?",
+             "What do you know about this company/organization?",
+             "Why should we hire you?",
+             "What is your greatest accomplishment?",
+             "What are your salary requirements?",
+             "Do you have any questions for us?",
+             "What are you looking for from a new position?",
+             "Are you considering other positions in other companies?",
+             "What is the professional achievement you're most proud of?",
+             "What kind of working environment do you work best in?",
+             "Where do you see yourself in 5 years?",
+             "How do you deal with pressure or stressful situations?",
+             "Do you think there is a difference between hard work and smart work?",
+             "How quickly do you adapt to new technology?",
+             "Do you have any interests outside of work?",
+             "Do you think our company/organization could do better?"]
 asked_questions = []
 
 
@@ -26,6 +46,7 @@ def main():
     print("Welcome to the interview program!")
     while len(questions) > 0:
         question = choose_random_question()
+        print(f"Question: {question}")
         text_to_speech(question)
         input("Type anything after speaking your answer ")
     text_to_speech("The interview has concluded!")
