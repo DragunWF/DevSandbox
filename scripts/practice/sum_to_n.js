378015a0541f62ad3ff2146a4d55674de482e282
dragunwf <plarisanmarc@gmail.com>
2023-07-26 00:08:40 +0800

    Update sum_to_n.js

--- scripts/practice/sum_to_n.js
+++ scripts/practice/sum_to_n.js
@@ -1,5 +1,3 @@
-const n = 6;
-
 function sumNumbers(n) {
   let sum = 0;
   for (let i = 1; i <= n; i++) {
@@ -12,5 +10,22 @@ function sumNumbersConstantTime(n) {
   return (n * (n + 1)) / 2;
 }
 
-console.log(sumNumbers(n));
-console.log(sumNumbersConstantTime(n));
+function showResult(func, arg) {
+  const t1 = performance.now();
+  console.log(`Result: ${func(arg)}`);
+  const t2 = performance.now();
+  console.log(`Time to execute: ${(t2 - t1) / 1000} seconds`);
+}
+
+function main() {
+  const n = 6;
+  const wall = "----------------------";
+  console.log(`n = ${n}`);
+
+  console.log(`${wall}\nFirst Function (Iteration)`);
+  showResult(sumNumbers, n);
+  console.log(`${wall}\nSecond Function (Formula)`);
+  showResult(sumNumbersConstantTime, n);
+}
+
+main();
