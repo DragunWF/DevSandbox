0040eddac729b8f08a8b188f9c411e1a3d2a7455
DragunDev <plarisanmarc@gmail.com>
2024-06-16 00:06:07 +0800

    Update anti_distraction.py

--- scripts/uncategorized/anti_distraction.py
+++ scripts/uncategorized/anti_distraction.py
@@ -4,13 +4,25 @@ import subprocess
 # Distracting programs to eliminate
 programs = ("spotify", "discord")  # Name of process in the 'ps' command
 INTERVAL = 60
-iteration = 0
+minutes_passed = 0
 
-while True:
-    iteration += 1
-    print(f"Iteration: {iteration}")
-    for program in programs:
-        command = f"ps {program} | kill"
-        subprocess.run(["powershell", "-Command", command],
-                       capture_output=True, text=True)
-    sleep(INTERVAL)
+
+def main() -> None:
+    while True:
+        minutes_passed += 1
+        minutes_str = f"{minutes_passed} Minute(s)"
+        if minutes_passed >= 60:
+            print(f"Time Passed: {minutes_str}")
+        else:
+            print(f"Time Passed: {minutes_passed // 60} Hours(s) and {minutes_str}")
+
+        for program in programs:
+            command = f"ps {program} | kill"
+            subprocess.run(["powershell", "-Command", command],
+                           capture_output=True, text=True)
+
+        sleep(INTERVAL)
+
+
+if __name__ == "__main__":
+    main()
