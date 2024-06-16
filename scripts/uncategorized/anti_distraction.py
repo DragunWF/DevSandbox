2b6d1aa8fab4cdcdd4e2f8e91d726d61d2b15c1c
DragunDev <plarisanmarc@gmail.com>
2024-06-16 17:52:51 +0800

    Update anti_distraction.py

--- scripts/uncategorized/anti_distraction.py
+++ scripts/uncategorized/anti_distraction.py
@@ -13,7 +13,7 @@ def main() -> None:
         if minutes_passed < 60:
             print(f"Time Passed: {minutes_passed} Minute(s)")
         else:
-            print(f"Time Passed: {minutes_passed // 60} Hours(s) and {minutes_passed % 60}")
+            print(f"Time Passed: {minutes_passed // 60} Hours(s) and {minutes_passed % 60} Minute(s)")
 
         for program in programs:
             command = f"ps {program} | kill"
