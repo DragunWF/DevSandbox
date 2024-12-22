724280062357feb8696caa88a0bed7ffd3646df1
DragunDev <plarisanmarc@gmail.com>
2024-12-23 00:42:56 +0800

    Update task_list project

--- django/task_list/main_app/views.py
+++ django/task_list/main_app/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import Account, Task
+from .models import Account, Task
 
 # Create your views here.
 
 
-def say_hello(request):
+def hello(request):
     # The 'objects' part of the Account returns a Manager object
     # that is used as an interface for the database. It has several
     # different methods to query objects. Furthermore, you can also
     # chain them. Example: query_set.filter().filter().order_by()
+
+    # Other methods include get(pk=1) which grabs the row with the
+    # matching primary key. Alternatively, the keyword parameter name
+    # can be the name of a column in the database like "username",
+    # "email", and others.
+
     query_set = Account.objects.all()
-    return str(list(query_set))
+    print(list(query_set))
+    return HttpResponse(str(list([obj.username for obj in query_set])))
