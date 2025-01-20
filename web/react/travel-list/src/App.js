01144f527ce31d3ecfd6b5f6853b6d0cdfe2de50
DragunDev <plarisanmarc@gmail.com>
2025-01-20 18:45:45 +0800

    Update travel-list react app

--- web/react/travel-list/src/App.js
+++ web/react/travel-list/src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Charger", quantity: 3, packed: true },
+  { id: 3, description: "Charger", quantity: 3, packed: true },
 ];
 
 function App() {
@@ -22,10 +22,43 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(event) {
+    // To prevent the page from reloading after submitting the form
+    event.preventDefault();
+    if (!description) {
+      return;
+    }
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(event) => setQuantity(Number(event.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num} onChange={test}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -34,7 +67,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
