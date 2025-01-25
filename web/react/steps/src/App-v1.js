d5c4d3c2f074eb33cfdf8eb4ccd05824d459730a
DragunDev <plarisanmarc@gmail.com>
2025-01-25 20:02:23 +0800

    Update steps react app

--- web/react/steps/src/App-v1.js
+++ web/react/steps/src/App-v1.js
@@ -36,36 +36,22 @@ function App() {
             Step {step}: {messages[step - 1]}
           </p>
           <div className="buttons">
-            <Button
-              textColor="#fff"
-              backgroundColor="#7950f2"
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
             >
-              👈 Previous
-            </Button>
-
-            <Button
-              textColor="#fff"
-              backgroundColor="#7950f2"
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handleNext}
             >
-              👉 Next
-            </Button>
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function Button({ textColor, backgroundColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: backgroundColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
 
