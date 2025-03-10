0a696c0479e4d7e0152554e92567e89bff637e3c
DragunDev <plarisanmarc@gmail.com>
2025-03-10 20:09:12 +0800

    Update FirstFragment android app

--- android/FragmentTest/app/src/main/java/com/example/fragmenttest/FirstFragment.java
+++ android/FragmentTest/app/src/main/java/com/example/fragmenttest/FirstFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +60,11 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        TextView nameText = view.findViewById(R.id.nameText);
+        nameText.setText("Marc L. Plarisan");
+
+        return view;
     }
 }
