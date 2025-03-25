be378a6e62d1d21249f6bde1ed8d42338204557a
DragunDev <plarisanmarc@gmail.com>
2025-03-25 14:55:32 +0800

    Update keyword_cipher.py

--- scripts/python/encryption/keyword_cipher.py
+++ scripts/python/encryption/keyword_cipher.py
@@ -1,11 +1,12 @@
 from rich import print
 from string import ascii_uppercase
 
-# Note: This is still in progress!
-
 
 def main() -> None:
+    print(create_cipher_map("COLLEGE"))
+
     print(encrypt("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z", "KRYPTOS"))
+    print(encrypt("UNIVERSITY", "COLLEGE"))  # UMFVGRSFTY
     print(encrypt("Knowledge is Power", "KRYPTOS"))  # IlmWjbaEb GQ NmWbp
     print(encrypt("zljeft dtOT", "secret"))  # ZOMBIE HERE
 
@@ -23,11 +24,11 @@ def encrypt(plain_text: str, keyword: str) -> str:
 
 
 def create_cipher_map(keyword: str) -> str:
-    output = str(set(keyword.upper()))
-    for char in ascii_uppercase:
-        if not char in keyword:
-            output += char
-    return output
+    cipher_map = []
+    for char in (keyword.upper() + ascii_uppercase):
+        if not char in cipher_map:
+            cipher_map.append(char)
+    return cipher_map
 
 
 if __name__ == "__main__":
