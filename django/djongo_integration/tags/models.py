d5c1187c99301b134583549df5677cbecb3c1f2d
DragunDev <plarisanmarc@gmail.com>
2025-04-17 13:41:33 +0800

    Course checkpoint

--- django/djongo_integration/tags/models.py
+++ django/djongo_integration/tags/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    # ContentType : allows the developer to create generic relationships between models
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
