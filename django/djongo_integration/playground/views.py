482801a98686f5a97a5a65c4309ebadb72f2641b
DragunDev <plarisanmarc@gmail.com>
2025-04-18 21:34:44 +0800

    Django app checkpoint

--- django/djongo_integration/playground/views.py
+++ django/djongo_integration/playground/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.conf import settings
-from django.db.models import Q, F
+from django.db.models import Q, F, Value, Func
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 
-from store.models import Product, Collection, OrderItem
+from decimal import Decimal
+from store.models import Product, Collection, OrderItem, Customer
 
 
 def say_hello(request):
     data = Product.objects.all()
+    result = Product.objects.aggregate(
+        count=Count("description"),
+    )
 
     return render(request, "hello.jinja",
-                  {'name': "Marc", "products": data})
+                  {'name': "Marc", "products": data, 'result': result})
