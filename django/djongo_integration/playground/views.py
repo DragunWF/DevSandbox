5e088dade6ad31ee4987e2d548126acf1b99fa85
DragunDev <plarisanmarc@gmail.com>
2025-04-18 21:46:31 +0800

    Create customer managers and update models

--- django/djongo_integration/playground/views.py
+++ django/djongo_integration/playground/views.py
@@ -4,16 +4,23 @@ from django.http import HttpResponse
 from django.conf import settings
 from django.db.models import Q, F, Value, Func
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from django.contrib.contenttypes.models import ContentType
 
 from decimal import Decimal
 from store.models import Product, Collection, OrderItem, Customer
+from tags.models import TaggedItem
 
 
 def say_hello(request):
-    data = Product.objects.all()
-    result = Product.objects.aggregate(
+    
+
+    products = Product.objects.values()[:25]
+    product_count = Product.objects.aggregate(
         count=Count("description"),
     )
 
     return render(request, "hello.jinja",
-                  {'name': "Marc", "products": data, 'result': result})
+                  {'name': "Marc",
+                   "products": products,
+                   'product_count': product_count,
+                   'tags': list(tags)})
--- django/djongo_integration/tags/models.py
+++ django/djongo_integration/tags/models.py
@@ -3,11 +3,31 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
+class TaggedItemManager(models.Manager):
+    """
+    Customer Query Manager
+    """
+
+    def get_tags_for(self, obj_type, obj_id):
+        """
+        Gets the tags from a product
+        """
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        return TaggedItem.objects.select_related("tag").filter(
+            content_type=content_type,
+            object_id=obj_id
+        )
+
+
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 
 class TaggedItem(models.Model):
+    # Overrides the default manager to a custom one
+    objects = TaggedItemManager()
+
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
     # ContentType : allows the developer to create generic relationships between models
