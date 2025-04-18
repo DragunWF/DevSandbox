cf746b2717f98d3f3726c61eeaaafc05b95f127c
DragunDev <plarisanmarc@gmail.com>
2025-04-18 16:08:19 +0800

    Django app checkpoint

--- django/djongo_integration/playground/views.py
+++ django/djongo_integration/playground/views.py
@@ -2,15 +2,26 @@ from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.conf import settings
-from django.db.models import Q
+from django.db.models import Q, F
 
-from store.models import Product
+from store.models import Product, Collection, OrderItem
 
 
 def say_hello(request):
-    # Your existing code
-    query_set = Product.objects.filter(
-        Q(inventory__lt=10) | Q(unit_price__lt=20))
+    order_items = [item[0]
+                   for item in OrderItem.objects.values_list("product")]
+    products = list(Product.objects.values())
+    ordered_products = []
+    assign_ids(products)
+    for product in products:
+        if product["id"] in order_items:
+            ordered_products.append(product)
+    ordered_products.sort(key=lambda product: product["title"])
 
     return render(request, "hello.jinja",
-                  {'name': "Marc", "products": list(query_set)})
+                  {'name': "Marc", "products": ordered_products})
+
+
+def assign_ids(data_set):
+    for i, item in enumerate(data_set):
+        item["id"] = i + 1
