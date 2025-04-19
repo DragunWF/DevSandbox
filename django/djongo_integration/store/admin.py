4b4274a0e3c8da47a74a52c09e45dc3691b53fd5
DragunDev <plarisanmarc@gmail.com>
2025-04-19 16:11:56 +0800

    Update admin site

--- django/djongo_integration/store/admin.py
+++ django/djongo_integration/store/admin.py
@@ -1,5 +1,36 @@
 from django.contrib import admin
+from django.db.models.aggregates import Count
+from django.urls import reverse
+from django.utils.http import urlencode
+from django.utils.html import format_html
+
 from . import models
+
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ["title", "collection_link", "products_count"]
+
+    def collection_link(self, collection):
+        url = (
+            reverse("admin:store_product_changelist")
+            + "?"
+            + urlencode({"collection__pk": collection._id})
+        )
+        return format_html('<a href="{}">Change Products</a>', url)
+
+    # Incompatible with Djongo
+    # def products_count(self, collection):
+    #     return collection.products_count
+
+    # def get_queryset(self, request):
+    #     return super().get_queryset(request).annotate(
+    #         products_count=Count("product")
+    #     )
+
+    def products_count(self, collection):
+        return collection.product_set.count()
+
+    # products_count.admin_order_field = "products_count"
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -17,6 +48,7 @@ class ProductAdmin(admin.ModelAdmin):
     def collection_title(self, product):
         return product.collection.title
 
+    # This decorator is incompatible with Djongo, refer to the alternative below
     # @admin.display(ordering="inventory")
     def inventory_status(self, product):
         if product.inventory < 10:
@@ -38,7 +70,7 @@ class OrderAdmin(admin.ModelAdmin):
     list_editable = ["payment_status"]
 
 
-admin.site.register(models.Collection)
+admin.site.register(models.Collection, CollectionAdmin)
 admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.Customer, CustomerAdmin)
 admin.site.register(models.Order, OrderAdmin)
