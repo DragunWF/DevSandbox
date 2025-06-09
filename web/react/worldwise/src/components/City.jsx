3427167ebb4a759d6837fa5dd8647747992c6b28
DragunDev <plarisanmarc@gmail.com>
2025-06-10 00:02:26 +0800

    Update worldwise react app

--- web/react/worldwise/src/components/City.jsx
+++ web/react/worldwise/src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -10,7 +10,10 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-  // const { id } = useParams();
+  const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
   // TEMP DATA
   const currentCity = {
@@ -25,6 +28,12 @@ function City() {
   return (
     <div className={styles.city}>
       <div className={styles.row}>
+        {/* ------ */}
+        <h1>City {id}</h1>
+        <p>
+          Position: {lat} {lng}
+        </p>
+        {/* ------ */}
         <h6>City name</h6>
         <h3>
           <span>{emoji}</span> {cityName}
