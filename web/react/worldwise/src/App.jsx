4fd7377c9efb71b3c823276d6d605cf0836e6bf2
DragunDev <plarisanmarc@gmail.com>
2025-06-09 16:19:06 +0800

    Update worldwise react app

--- web/react/worldwise/src/App.jsx
+++ web/react/worldwise/src/App.jsx
@@ -1,3 +1,20 @@
-export default function App() {
-  return <div>World Wise</div>;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
