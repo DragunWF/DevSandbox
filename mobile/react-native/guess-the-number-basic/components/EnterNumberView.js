3eb57d5e3df089e45bc5f9321f7d2f93db8f887c
DragunDev <plarisanmarc@gmail.com>
2025-06-18 18:46:16 +0800

    Create EnterNumberView.js

---
+++ mobile/react-native/guess-the-number-basic/components/EnterNumberView.js
@@ -0,0 +1,9 @@
+import { StyleSheet, View } from "react-native";
+
+function EnterNumberView() {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({});
+
+export default EnterNumberView;
