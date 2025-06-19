0f4d7b4af2bd06e4a3ea7286d904f1e39a642831
DragunDev <plarisanmarc@gmail.com>
2025-06-19 23:49:42 +0800

    Update react native app

--- mobile/react-native/mini-guessing-game/screens/GameScreen.jsx
+++ mobile/react-native/mini-guessing-game/screens/GameScreen.jsx
@@ -2,12 +2,24 @@ import { StyleSheet, View, Text } from "react-native";
 
 function GameScreen() {
   return (
-    <View>
-      <Text>Game Screen!</Text>
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* Guess */}
+      <View>
+        <Text>Higher or lower</Text>
+        {/* + */}
+        {/* - */}
+      </View>
+      <View>LOG ROUNDS</View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
 
 export default GameScreen;
