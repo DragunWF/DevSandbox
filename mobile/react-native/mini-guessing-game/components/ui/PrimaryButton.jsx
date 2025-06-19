4888f50fd991266ed1df7440bd3aa1a282a3b224
DragunDev <plarisanmarc@gmail.com>
2025-06-20 00:14:55 +0800

    Update mini-guessing-game react native app

--- mobile/react-native/mini-guessing-game/components/ui/PrimaryButton.jsx
+++ mobile/react-native/mini-guessing-game/components/ui/PrimaryButton.jsx
@@ -1,6 +1,6 @@
 import { StyleSheet, View, Pressable, Touchable, Text } from "react-native";
 
-import Colors from "../constants/colors";
+import Colors from "../../constants/colors";
 
 function PrimaryButton({ children, onPress }) {
   return (
--- mobile/react-native/mini-guessing-game/screens/StartGameScreen.jsx
+++ mobile/react-native/mini-guessing-game/screens/StartGameScreen.jsx
@@ -8,7 +8,7 @@ import {
   Alert,
 } from "react-native";
 
-import PrimaryButton from "../components/PrimaryButton";
+import PrimaryButton from "../components/ui/PrimaryButton";
 import Colors from "../constants/colors";
 
 function StartGameScreen({ onPickNumber }) {
