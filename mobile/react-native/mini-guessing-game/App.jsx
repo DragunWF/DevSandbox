4ae17ce18117405258af18ec29f7733958e3bd69
DragunDev <plarisanmarc@gmail.com>
2025-06-19 23:30:58 +0800

    Update react native app

--- mobile/react-native/mini-guessing-game/App.jsx
+++ mobile/react-native/mini-guessing-game/App.jsx
@@ -1,13 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <LinearGradient colors={["#4e0329", "ddb52f"]} style={styles.rootScreen}>
-      <StartGameScreen />
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <StatusBar style="light" />
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -15,5 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
