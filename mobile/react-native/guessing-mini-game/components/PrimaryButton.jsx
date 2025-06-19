11e3c6fa9391b23c518bba70c1d7e530cea24645
DragunDev <plarisanmarc@gmail.com>
2025-06-19 19:49:52 +0800

    Update react native app

---
+++ mobile/react-native/guessing-mini-game/components/PrimaryButton.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, View, Pressable, Text } from "react-native";
+
+function PrimaryButton({ children }) {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+}
+
+export default PrimaryButton;
---
+++ mobile/react-native/guessing-mini-game/screens/StartGameScreen.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Button, TextInput, Pressable } from "react-native";
+
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
