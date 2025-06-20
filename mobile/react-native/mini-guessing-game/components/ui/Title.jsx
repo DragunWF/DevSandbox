787fd9def33db1066fa58c98635c50c2bc43ac64
DragunDev <plarisanmarc@gmail.com>
2025-06-20 14:21:50 +0800

    Update mini-guessing-game react native app

--- mobile/react-native/mini-guessing-game/components/ui/Title.jsx
+++ mobile/react-native/mini-guessing-game/components/ui/Title.jsx
@@ -1,5 +1,4 @@
 import { StyleSheet, Text } from "react-native";
-import Colors from "../../constants/colors";
 
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -7,6 +6,7 @@ function Title({ children }) {
 
 const styles = StyleSheet.create({
   title: {
+    fontFamily: "open-sans-bold",
     fontSize: 18,
     fontWeight: "bold",
     color: "white",
