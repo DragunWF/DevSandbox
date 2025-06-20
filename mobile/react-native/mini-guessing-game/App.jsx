3beff40b5f5b5bb4df5e2557d9b26287939176c6
DragunDev <plarisanmarc@gmail.com>
2025-06-20 17:24:40 +0800

    Update mini-guessing-game react native app

--- mobile/react-native/mini-guessing-game/App.jsx
+++ mobile/react-native/mini-guessing-game/App.jsx
@@ -37,8 +37,9 @@ export default function App() {
 
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
-  function gameOverHandler() {
+  function gameOverHandler(numberOfRounds) {
     setGameIsOver(true);
+    setGuessRounds(numberOfRounds);
   }
 
   function startNewGameHandler() {
--- mobile/react-native/mini-guessing-game/screens/GameScreen.jsx
+++ mobile/react-native/mini-guessing-game/screens/GameScreen.jsx
@@ -29,7 +29,7 @@ function GameScreen({ userNumber, onGameOver }) {
 
   useEffect(() => {
     if (currentGuess === userNumber) {
-      onGameOver();
+      onGameOver(guessRounds.length);
     }
   }, [currentGuess, userNumber, onGameOver]);
 
@@ -87,7 +87,7 @@ function GameScreen({ userNumber, onGameOver }) {
           </View>
         </View>
       </Card>
-      <View>
+      <View style={styles.listContainer}>
         <FlatList
           data={guessRounds}
           renderItem={(guessRoundData) => {
@@ -119,6 +119,10 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
+  },
+  listContainer: {
+    flex: 1,
+    padding: 16,
   },
 });
 
